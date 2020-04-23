1fb44f7f9fb50aa9dc14abbebfb5fc6c22912934
Christian Hutto <chutto@ChristiansMBP2.attlocal.net>
2020-04-23 16:16:54 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,24 +1,27 @@
 console.log("Up and running!");
 
-const cards = ["queen", "queen", "king", "king"];
+let cards = ["queen", "queen", "king", "king"];
 
-const cardsInPlay = [];
+let cardsInPlay = [];
 
-const cardOne = cards[0];
+let cardId;
 
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
-const cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-
-if(cardsInPlay[0] === cardsInPlay[2]) {
-	alert("You found a match!");
+function checkForMatch() {
+	if(cardsInPlay.length === 2) {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
 } else {
-	alert("Sorry, try again.")
+  console.log("Sorry, try again.");
 }
+}};
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+flipCard(0);
+flipCard(2);
+
+
+
